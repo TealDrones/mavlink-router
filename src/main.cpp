26c83cd2384813de724674248b6fd73e9cd0e96f
Greivin Fallas <greivin.fallas@ridgerun.com>
2020-01-19 14:26:28 -0600

    Increase version number

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@
 
 #define DEFAULT_CONFFILE "/etc/dcm/main.conf"
 #define DEFAULT_CONF_DIR "/etc/dcm/config.d"
-#define MAIN_VERSION "0.0.6"
+#define MAIN_VERSION "0.0.7"
 
 struct options {
     const char *filename;
