254f5b8c45f20895c9688a58f1e4c0b49f1734dd
Michael Hunter <michael.hunter@tealdrones.com>
2020-03-13 15:56:04 -0600

    Updated to latest and updated configuration

--- buildMav.sh
+++ buildMav.sh
@@ -2,7 +2,7 @@ source /usr/local/rb3-oecore-x86_64/environment-setup-aarch64-oe-linux
 make clean
 python ./modules/mavlink/pymavlink/tools/mavgen.py -o include/mavlink --lang C --wire-protocol 2.0 modules/mavlink/message_definitions/v1.0/ardupilotmega.xml
 if make -j10; then
-    cp mavlink-routerd ~/code/distro
+    scp mavlink-routerd root@drone:~/code/distro/
     echo "+++++++++++++ Success "
 else
     echo "------------- Fail "
