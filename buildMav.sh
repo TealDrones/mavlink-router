3a6280a69297c6708315f7fb58e5263ce146a7b1
Michael Hunter <michael.hunter@tealdrones.com>
2020-05-29 21:23:32 -0600

    added autogen for jenkins buils

--- buildMav.sh
+++ buildMav.sh
@@ -1,4 +1,7 @@
 source /usr/local/rb3-oecore-x86_64/environment-setup-aarch64-oe-linux
+./autogen.sh && ./configure CFLAGS='-g -O2' \
+        --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib64 \
+    --prefix=/usr
 make clean
 python ./modules/mavlink/pymavlink/tools/mavgen.py -o include/mavlink --lang C --wire-protocol 2.0 modules/mavlink/message_definitions/v1.0/ardupilotmega.xml
 if make -j10; then
