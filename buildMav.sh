68e5c1c82a8124d82064e0a300c8d5ccf55172ac
Michael Hunter <michael.hunter@tealdrones.com>
2020-06-01 09:57:20 -0600

    buildscript enhancement

--- buildMav.sh
+++ buildMav.sh
@@ -1,13 +1,17 @@
 source /usr/local/rb3-oecore-x86_64/environment-setup-aarch64-oe-linux
-./configure CFLAGS='-g -O2' CXXFLAGS='-g -O2' --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib          --with-rootprefix=       --with-rootlibdir=/lib 
-./autogen.sh && ./configure CFLAGS='-g -O2' \
-        --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib64 \
+./autogen.sh
+./configure CFLAGS='-g -O2' \
+    --sysconfdir=/etc \
+    --localstatedir=/var \
+    --libdir=/usr/lib64 \
     --prefix=/usr
 make clean
 python ./modules/mavlink/pymavlink/tools/mavgen.py -o include/mavlink --lang C --wire-protocol 2.0 modules/mavlink/message_definitions/v1.0/ardupilotmega.xml
 if make -j10; then
-    scp mavlink-routerd root@drone:~/code/distro/
     echo "+++++++++++++ Success "
+    scp mavlink-routerd pi@dmz.tealdrones.com:~/code/distro/
+    exit 0
 else
     echo "------------- Fail "
+    exit 1
 fi
