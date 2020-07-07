21234288cb3df12cda70fb65c4b0a0d559ae2991
Colby Whitney <colby.whitney@tealdrones.com>
2020-07-07 11:35:33 -0600

    Adding udp crc check back
    I've been testing and analyzing and am not seeing any issues with the
    UDP crc checks as it relates to the camera manager.  All messages from
    qground to control the gimbal and camera functions are working.  And the
    reporting of gimbal status, available sd card space, etc. are all
    working.  As such I've added the udp check back in with one error log
    (which I'm never seeing in the output right now) to catch it if it does
    start to become a problem.

--- src/mavlink-router/endpoint.cpp
+++ src/mavlink-router/endpoint.cpp
@@ -223,18 +223,12 @@ int Endpoint::read_msg(struct buffer *pbuf, int *target_sysid, int *target_compi
          * Ground Station and Flight Stack. Although it can also be a
          * corrupted message is better forward than silent drop it.
          */
-        /* don't check CRC of UDP packets for now */
-        if (this->_name.compare("UDP") != 0) {
-            if (!_check_crc(msg_entry)) {
-                _stat.read.crc_error++;
-                _stat.read.crc_error_bytes += expected_size;
-                return 0;
-            }
-        } else {
-            if (!_check_crc(msg_entry)) {
-                log_debug("UDP CRC issue %u", *msg_id);
-            }
+        if (!_check_crc(msg_entry)) {
+            _stat.read.crc_error++;
+            _stat.read.crc_error_bytes += expected_size;
+            return 0;
         }
+        
         _add_sys_comp_id(((uint16_t)*src_sysid << 8) | *src_compid);
     }
 
@@ -375,21 +369,17 @@ void Endpoint::postprocess_msg(int target_sysid, int target_compid, uint8_t src_
 
 bool Endpoint::_check_crc(const mavlink_msg_entry_t *msg_entry)
 {
-    log_info("_check_crc ");
-
     const bool mavlink2 = rx_buf.data[0] == MAVLINK_STX;
     uint16_t crc_msg, crc_calc;
     uint8_t payload_len, header_len, *payload;
 
     if (mavlink2) {
-        log_info("_check_crc mavlink2 ");
         struct mavlink_router_mavlink2_header *hdr =
                     (struct mavlink_router_mavlink2_header *)rx_buf.data;
         payload = rx_buf.data + sizeof(*hdr);
         header_len = sizeof(*hdr);
         payload_len = hdr->payload_len;
     } else {
-        log_info("_check_crc mavlink1 ");
         struct mavlink_router_mavlink1_header *hdr =
                     (struct mavlink_router_mavlink1_header *)rx_buf.data;
         payload = rx_buf.data + sizeof(*hdr);
@@ -401,7 +391,7 @@ bool Endpoint::_check_crc(const mavlink_msg_entry_t *msg_entry)
     crc_calc = crc_calculate(&rx_buf.data[1], header_len + payload_len - 1);
     crc_accumulate(msg_entry->crc_extra, &crc_calc);
     if (crc_calc != crc_msg) {
-        log_info("_check_crc calc:%d message:%d", crc_calc, crc_msg);
+        log_error("crc check failed for message id(%d).  calc crc(%d) message crc(%d)", msg_entry->msgid, crc_calc, crc_msg);
         return false;
     }
 
