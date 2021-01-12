9d8201619eb4a9eb105ddb6b6e9d99c2051866cb
Matt McFadden <matt.mcfadden@tealdrones.com>
2021-01-12 10:11:21 -0700

    SECL:Tweaks to build script

--- buildMav.sh
+++ buildMav.sh
@@ -1,4 +1,4 @@
-source /usr/local/rb3-oecore-x86_64/environment-setup-aarch64-oe-linux
+source /usr/local/teal-oecore-x86_64/environment-setup-aarch64-oe-linux
 ./autogen.sh
 ./configure CFLAGS='-g -O2' \
     --sysconfdir=/etc \
@@ -10,7 +10,7 @@ make clean
 python ./modules/mavlink/pymavlink/tools/mavgen.py -o include/mavlink --lang C --wire-protocol 2.0 modules/mavlink/message_definitions/v1.0/ardupilotmega.xml
 if make -j10; then
     echo "+++++++++++++ Success "
-    scp mavlink-routerd pi@dmz.tealdrones.com:~/code/distro/
+    #scp mavlink-routerd pi@dmz.tealdrones.com:~/code/distro/
     exit 0
 else
     echo "------------- Fail "
